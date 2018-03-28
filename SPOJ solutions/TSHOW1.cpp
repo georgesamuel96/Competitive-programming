1eca9b86b4c8d0b2693fa068edc345e04cf25172
georgesamuel96 <georges012441@gmail.com>
2018-03-28 03:26:23 +0200

    commit

--- SPOJ solutions/TSHOW1.cpp
+++ SPOJ solutions/TSHOW1.cpp
@@ -1,41 +1,116 @@
 #define _CRT_SECURE_NO_WARNINGS
-/* Dynamic Programming C/C++ implementation of LIS problem */
-#include<stdio.h>
-#include<stdlib.h>
+#include<iostream>
+#include<sstream>
+#include<vector>
+#include<algorithm>
+#include<string.h>
+#include<set>
+#include<fstream>
+#include<map>
+#include<string>
+#include <stdio.h>
+#include<bitset>
+#include<queue>
+#include<iomanip>
+#include<cmath>
+#include<stack>
+#include<climits>
 
-/* lis() returns the length of the longest increasing
-subsequence in arr[] of size n */
-int lis(int arr[], int n)
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll>vl;
+typedef vector<int>vi;
+typedef vector<bool>vb;
+typedef vector<double>vd;
+typedef vector<char>vc;
+typedef vector<string>vs;
+#define MP make_pair
+#define all(v) v.begin(),v.end()
+#define EPS 1e-9
+#define PI 3.14159265359
+const ll mod = 1000000007;
+const ll OO = (ll)1e18;
+const int dx[] = { 0, 1, -1, 0, 1, -1, 1, -1 };
+const int dy[] = { 1, 0, 0, -1, 1, -1, -1, 1 };
+ll gcd(ll a, ll b){ if (b == 0){ return a; }return gcd(b, a % b); }
+ll fast_power(double base, ll power){
+	if (power == 1) return base;
+	if (power % 2 == 0) return fast_power((base*base), power / 2);
+	else return(base*fast_power((base*base), power / 2));
+}
+//#pragma warning (disable : 4996)
+void Qalbaz()
 {
-	int *lis, i, j, max = 0;
-	lis = (int*)malloc(sizeof(int) * n);
-
-	/* Initialize LIS values for all indexes */
-	for (i = 0; i < n; i++)
-		lis[i] = 1;
-
-	/* Compute optimized LIS values in bottom up manner */
-	for (i = 1; i < n; i++)
-		for (j = 0; j < i; j++)
-			if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
-				lis[i] = lis[j] + 1;
-
-	/* Pick maximum of all LIS values */
-	for (i = 0; i < n; i++)
-		if (max < lis[i])
-			max = lis[i];
-
-	/* Free memory to avoid memory leak */
-	free(lis);
-
-	return max;
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cout << fixed << setprecision(4);
 }
 
-/* Driver program to test above function */
-int main()
-{
-	int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	printf("Length of lis is %dn", lis(arr, n));
+
+int main(){
+
+	Qalbaz();
+
+	vl powers, sum;
+	ll p = 2;
+	sum.push_back(0);
+	powers.push_back(0);
+	while (sum[sum.size() - 1] <= 1e15)
+	{
+		powers.push_back(p);
+		p = p * 2LL;
+		sum.push_back(powers[powers.size() - 1] + sum[sum.size() - 1]);
+	}
+	int t, test = 1;
+	cin >> t;
+	ll k;
+	while (t--)
+	{
+		cin >> k;
+
+		ll index = 0;
+		while (sum[index] < k)
+			index++;
+		ll from = sum[index - 1] + 1, to = sum[index], mid, ans;
+		while (from <= to)
+		{
+			mid = (from + to) / 2;
+			if (mid < k)
+				from = mid + 1;
+			else if (mid > k)
+				to = mid - 1;
+			else
+			{
+				ans = mid;
+				break;
+			}
+		}
+		string str;
+		ans -= sum[index - 1] + 1;
+
+		while (ans)
+		{
+			if (ans % 2 == 0)
+				str.push_back('5');
+			else
+				str.push_back('6');
+			ans /= 2;
+		}
+		int numberOfDigits = 0;
+		index = powers[index] - 1;
+		while (index)
+			numberOfDigits++, index /= 2;
+		while (str.size() < numberOfDigits)
+			str.push_back('5');
+		for (int i = str.size() - 1; i >= 0; i--)
+			cout << str[i];
+
+		cout << "\n";
+	}
+
 	return 0;
 }
