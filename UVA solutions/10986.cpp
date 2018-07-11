9484c2b39a70db2da819cbf5ee0e81e0b4df4a60
georgesamuel96 <georges012441@gmail.com>
2018-07-11 16:20:05 +0200

    commit

--- UVA solutions/10986.cpp
+++ UVA solutions/10986.cpp
@@ -28,19 +28,22 @@ typedef vector<char>vc;
 typedef vector<string>vs;
 #define MP make_pair
 #define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
 #define EPS 1e-9
 #define PI 3.14159265359
 const ll mod = 1000000007;
-const ll OO = (ll)1e9;
-const int dx[] = { 0, 1, -1, 0, 1, -1, 1, -1 };
-const int dy[] = { 1, 0, 0, -1, 1, -1, -1, 1 };
+const ll OO = (ll)1e18;
+const int dx[] = { 0, 1, 0, -1, 1, -1, 1, -1 };
+const int dy[] = { 1, 0, -1, 0, 1, -1, -1, 1 };
 ll gcd(ll a, ll b){ if (b == 0){ return a; }return gcd(b, a % b); }
 ll fast_power(ll base, ll power){
+	if (power == 0)
+		return 1;
 	if (power == 1)
-		return base;
+		return (base % mod);
 	if (power % 2 == 0)
-		return fast_power((base*base), power / 2);
-	else return(base*fast_power((base*base), power / 2));
+		return (fast_power(((base*base) % mod), power / 2LL) % mod);
+	else return ((base*fast_power(((base*base) % mod) % mod, power / 2LL)) % mod);
 }
 //#pragma warning (disable : 4996)
 void Qalbaz()
@@ -50,53 +53,49 @@ void Qalbaz()
 	freopen("output.txt", "w", stdout);
 #endif
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	cout << fixed << setprecision(4);
+	cout << fixed << setprecision(2);
 }
-
-vi vis;
-vector<vector<pair<int, int>>>adj;
-priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
-int solve(int src, int dist, int n)
+ll dist[20000];
+vector<vector<pair<int, ll>>>adj;
+int n, m, src, dis;
+priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>>pq;
+ll dijkstra()
 {
-	while (!pq.empty())
-		pq.pop();
-	vis.clear();
-	vis.resize(n, OO);
+	for (int i = 0; i < 20000; i++)
+		dist[i] = OO;
+	dist[src] = 0;
 	pq.push(MP(0, src));
-	vis[src] = 0;
-	pair<int, int>v, u;
+	int d, u;
+	pair<ll, int>v;
 	while (!pq.empty())
 	{
-		v = pq.top();
+		d = pq.top().first;
+		u = pq.top().second;
 		pq.pop();
-		if (v.second == dist)
-			return v.first;
-		if (v.first > vis[v.second])
+		if (d > dist[u])
 			continue;
-		for (int i = 0; i < adj[v.second].size(); i++)
+		for (int i = 0; i < adj[u].size(); i++)
 		{
-			u = adj[v.second][i];
-			if (v.first + u.second < vis[u.first])
+			v = adj[u][i];
+			if (d + v.second < dist[v.first])
 			{
-				vis[u.first] = v.first + u.second;
-				pq.push(MP(vis[u.first], u.first));
+				dist[v.first] = d + v.second;
+				pq.push(MP(dist[v.first], v.first));
 			}
 		}
 	}
-	return -1;
+	return dist[dis] != OO? dist[dis] : -1;
 }
-
 int main(){
 
 	Qalbaz();
 
-	int t;
-	int test = 0;
+	int t, u, v, test = 1;
+	ll w;
 	cin >> t;
-	int n, m, src, dist, u, v, w;
 	while (t--)
 	{
-		cin >> n >> m >> src >> dist;
+		cin >> n >> m >> src >> dis;
 		adj.clear();
 		adj.resize(n);
 		for (int i = 0; i < m; i++)
@@ -105,12 +104,11 @@ int main(){
 			adj[u].push_back(MP(v, w));
 			adj[v].push_back(MP(u, w));
 		}
-		int ans = solve(src, dist, n);
-		cout << "Case #" << ++test << ": ";
-		if (ans == -1)
-			cout << "unreachable\n";
+		int ans = dijkstra();
+		if (ans != -1)
+		cout << "Case #" << test++ << ": " << ans << "\n";
 		else
-			cout << ans << "\n";
+			cout << "Case #" << test++ << ": " << "unreachable" << "\n";
 	}
 
 	return 0;
